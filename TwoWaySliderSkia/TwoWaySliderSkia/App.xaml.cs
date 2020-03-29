f92cc1f56eda550684217e3301f6b7ede5a32d9f
Akshay Kulkarni <M9012619@mindtree.com>
2020-03-29 21:51:49 +0530

    Add project files.

---
+++ TwoWaySliderSkia/TwoWaySliderSkia/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using TwoWaySliderSkia.Services;
+using TwoWaySliderSkia.Views;
+
+namespace TwoWaySliderSkia
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
